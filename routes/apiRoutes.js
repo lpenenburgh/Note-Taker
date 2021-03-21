6c37747c37433fc1ccf0fd876675597c9398cc27
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-03-21 17:25:10 -0600

    api route get and post added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,20 @@ const { v4: uuidv4 } = require('uuid');
 const db = require("../db/db.json")
 
 
-module.exports = function (app) {
+module.exports = (app) => {
+    app.get("/api/notes", (req, res) => {
+        res.send(db);
+    });
 
+    app.post("/api/notes", (req, res) => {
+        let newNote = {
+            id: uuidv4(),
+            title: req.body.title,
+            text: req.body.text
+
+        };
+        db.push(newNote);
+        res.send(newNote);
+    });
 
 };
