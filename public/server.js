11567581dd8b131e4222220547bd7e0d7e55cd41
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-03-21 15:45:12 -0600

    server.js listening

---
+++ public/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'))
+
+
+
+require('../routes/apiRoutes')(app);
+require('../routes/htmlroutes')(app);
+
+
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
